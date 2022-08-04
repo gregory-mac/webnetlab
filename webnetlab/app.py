ce25a0e62a6b9a7d7b07e91378c36e2482742033
Macheev <macheev@selectel.ru>
2022-08-04 11:24:45 +0300

    Changes in app.py

--- webnetlab/app.py
+++ webnetlab/app.py
@@ -1,13 +1,23 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from fastapi.templating import Jinja2Templates
+from .settings import settings
+from uvicorn import run
 
-app = FastAPI()
+from views.lab_screen import router as lab_screen_router
+
+
+def get_templates():
+    return Jinja2Templates(directory="templates")
+
+
+app = FastAPI(dependencies=Depends(get_templates))
+app.include_router(lab_screen_router)
 
 
 @app.get("/")
-async def root():
+def root():
     return {"message": "Hello World"}
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+if __name__ == "__main__":
+    run("app:app", host="0.0.0.0", port=8080, workers=1, reload=True)
