396400c51c7e0cdc4fe58f7d2061910f7604c7c3
Macheev <macheev@selectel.ru>
2022-08-15 00:07:54 +0300

    Added alembic

---
+++ webnetlab/migration/versions/03ba2956af0e_initial.py
@@ -0,0 +1,31 @@
+"""initial
+
+Revision ID: 03ba2956af0e
+Revises: 
+Create Date: 2022-08-15 00:04:16.184981
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+from sqlalchemy import Integer, String, Boolean
+
+# revision identifiers, used by Alembic.
+revision = '03ba2956af0e'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    op.create_table(
+        'users',
+        sa.Column("id", Integer, primary_key=True),
+        sa.Column("login", String(30), nullable=False, unique=True),
+        sa.Column("email", String(50), nullable=False, unique=True),
+        sa.Column("is_admin", Boolean, nullable=False, unique=False, default=False),
+    )
+
+
+def downgrade() -> None:
+    pass
