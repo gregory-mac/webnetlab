b2435ad560df4c0fd3af3bab306172bd86e02d92
Macheev <macheev@selectel.ru>
2022-08-14 13:52:45 +0300

    Fixed lab list path

--- webnetlab/core/views/lab_list.py
+++ webnetlab/core/views/lab_list.py
@@ -11,7 +11,7 @@ from core.settings import settings
 router = APIRouter(prefix="/list")
 
 
-@router.get("/lab-list", status_code=200, response_class=HTMLResponse)
+@router.get("/", status_code=200, response_class=HTMLResponse)
 def list_labs_view(request: Request):
     lab_dir_list = scan_for_lab_folders(settings.path_to_lab_files)
     return templates.TemplateResponse("list_labs.html", {"request": request, "lab_dir_list": lab_dir_list})
