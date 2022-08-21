b83d23b615b8aedf69afa6bc8249f160be4d43a8
Macheev <macheev@selectel.ru>
2022-08-21 17:56:11 +0300

    New list_labs tmp and func

--- webnetlab/core/views/lab_list.py
+++ webnetlab/core/views/lab_list.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 from starlette.requests import Request
 from starlette.responses import HTMLResponse
 
@@ -13,5 +15,17 @@ router = APIRouter(prefix="/list")
 
 @router.get("/", status_code=200, response_class=HTMLResponse)
 def list_labs_view(request: Request):
+    lab_table = {}
     lab_dir_list = functions.lab.scan_for_lab_folders(settings.path_to_lab_files)
-    return dependencies.templates.TemplateResponse("list_labs.html", {"request": request, "lab_dir_list": lab_dir_list})
+    for lab in lab_dir_list:
+        lab_table[lab] = {}
+        lab_details = functions.lab.parse_lab_specification(Path(settings.path_to_lab_files) / lab / settings.lab_spec_filename)
+        try:
+            lab_table[lab]["author"] = lab_details["author"]
+        except KeyError:
+            lab_table[lab]["author"] = "---"
+        try:
+            lab_table[lab]["difficulty"] = lab_details["difficulty"]
+        except KeyError:
+            lab_table[lab]["difficulty"] = "---"
+    return dependencies.templates.TemplateResponse("list_labs.html", {"request": request, "lab_table": lab_table})
