23313d37ff2bc5ae58bddd5589d207c4e25ef14f
Macheev <macheev@selectel.ru>
2022-08-27 17:10:56 +0300

    Added status check

--- webnetlab/core/static/js/lab_controls.js
+++ webnetlab/core/static/js/lab_controls.js
@@ -10,6 +10,14 @@ function unlock_deploy_button() {
 	$("#deploy").removeClass('disabled');
 }
 
+function lock_destroy_button() {
+	$("#destroy").addClass('disabled');
+}
+
+function unlock_destroy_button() {
+	$("#destroy").removeClass('disabled');
+}
+
 $(function(){
 	$("#deploy").on("click", function(){
 		lock_deploy_button();
@@ -21,8 +29,8 @@ $(function(){
                 if (response.done === false) {
                     isDone(Date.now());
                 } else {
-                    console.log(response);
 					unlock_deploy_button();
+					update_status();
                 }
             },
 			error: function(error){
@@ -32,14 +40,6 @@ $(function(){
 		});
 	});
 });
-
-function lock_destroy_button() {
-	$("#destroy").addClass('disabled');
-}
-
-function unlock_destroy_button() {
-	$("#destroy").removeClass('disabled');
-}
 
 $(function(){
 	$("#destroy").on("click", function(){
@@ -52,8 +52,8 @@ $(function(){
                 if (response.done === false) {
                     isDone(Date.now());
                 } else {
-                    console.log(response);
 					unlock_destroy_button();
+					update_status();
                 }
             },
 			error: function(error){
@@ -64,15 +64,37 @@ $(function(){
 	});
 });
 
-function check_status() {
+function update_status() {
+	let current_status = $("#lab-status");
+	let current_status_indicator = current_status.children();
+
 	$.ajax({
 		url: "/lab/status",
 		type: "POST",
+		dataType: "json",
 		success: function(response) {
+			if (response["is_running"]===true){
+				current_status.text("Lab status: online (" + response["lab_name"] + ")");
+				current_status.prepend(current_status_indicator);
+				$("#lab-status-indicator")
+					.css('color', 'ForestGreen')
+					.addClass("tada animated infinite");
+			} else {
+				current_status.text("Lab status: offline");
+				current_status.prepend(current_status_indicator);
+				$("#lab-status-indicator")
+					.css('color', 'coral')
+					.removeClass("tada animated infinite");
+			}
 			return response;
 		},
 		error: function(error){
 			console.log(error);
+			current_status.text("Lab status: unknown");
+			current_status.prepend(current_status_indicator);
+			$("#lab-status-indicator")
+				.css('color', 'gray')
+				.removeClass("tada animated infinite");
 			return error;
 		}
 	});
